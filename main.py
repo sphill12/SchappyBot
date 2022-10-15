db858b76e3a1996abf847a3fbc24c85beab552f8
jschappel <jmschappel12@gmail.com>
2022-10-14 20:12:26 -0400

    chore: Added requirements.txt for easy install

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import random
 from time import sleep
 import requests
 
-load_dotenv()                                                                                                                                                                                                                            
+load_dotenv() #load in env vars
+
+
 bot = discord.Bot(intents=discord.Intents.all())
 
 @bot.event
@@ -16,11 +18,9 @@ async def on_ready():
 @bot.slash_command()
 async def hello(ctx, name: str = None):
     name = name or ctx.author.name
-    print('before response ')
-    
     await ctx.respond(f"Hello {name}!")
     
-    print('after response')
+
 
 @bot.command()
 async def gtn(ctx):
@@ -28,14 +28,12 @@ async def gtn(ctx):
     guess = None
     await ctx.respond('Guess a number between 1 and 10.')
     guess = await bot.wait_for('message', check=lambda message: message.author == ctx.author)
-    print(guess)
     if int(guess.content) == 5:
-          await ctx.send('You guessed it!')
-     else:
-          await ctx.send('Nope, try again.')
-          
-@bot.command()
+        await ctx.send('You guessed it!')
+    else:
+        await ctx.send('Nope, try again.')
 
+        
 
 bot.run(os.getenv('TOKEN'))
 
